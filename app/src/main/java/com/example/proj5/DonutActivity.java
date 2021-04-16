48b396a6ed25f2be50f84e418c9bbefda4b5783d
James Aikins <aikinsjk@gmail.com>
2021-04-16 16:47:54 -0400

    Started Activity files

---
+++ app/src/main/java/com/example/proj5/DonutActivity.java
@@ -0,0 +1,15 @@
+package com.example.proj5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DonutActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_donut_view);
+
+
+    }
+}
--- app/src/main/java/com/example/proj5/MainActivity.java
+++ app/src/main/java/com/example/proj5/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.proj5;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent donutView = new Intent(this,DonutActivity.class);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button openDonuts = findViewById(R.id.openDonuts);
+        openDonuts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(donutView);
+            }
+        });
     }
 }
